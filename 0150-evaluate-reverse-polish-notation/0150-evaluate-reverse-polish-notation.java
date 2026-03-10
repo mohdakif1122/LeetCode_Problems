b1de990b79c835f35ce73826cdffb419a3a36388
MOHD AKIF SIDDIQ <akifsiddiq28@gmail.com>
2026-03-11 01:43:35 +0530

    Time: 6 ms (90.61%), Space: 45.4 MB (43.84%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> res = new Stack<>();
+        int x,y;
+
+        for(String tk : tokens){
+            if(tk.equals("+")){
+                res.push(res.pop() + res.pop());
+            }
+            else if (tk.equals("-")){
+                y = res.pop();
+                x = res.pop();
+                res.push(x-y);
+            }
+            else if (tk.equals("/")){
+                y = res.pop();
+                x = res.pop();
+                res.push(x/y);
+            }
+            else if (tk.equals("*")){
+                res.push(res.pop() * res.pop());
+            }
+            else{
+                res.push(Integer.parseInt(tk));
+            }
+        }
+        return res.pop();
+    }
+}
