9c4cbe4ef8adfd0611c6a694ca8695e1a19acc24
MOHD AKIF SIDDIQ <akifsiddiq28@gmail.com>
2026-03-15 01:57:22 +0530

    Time: 35 ms (14.78%), Space: 54.5 MB (56.68%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,24 @@
+class StockSpanner {
+    Stack<int[]> stk;   //declare
+    public StockSpanner() {
+        stk = new Stack<>();    //initialize
+
+    }
+    
+    public int next(int price) {
+        int count = 1;
+        if(stk.isEmpty()){
+            stk.push(new int[]{price , count});
+            return count;
+        }
+        else{
+            while(!stk.isEmpty() && stk.peek()[0] <= price){
+                count += stk.peek()[1];
+                stk.pop();
+            }
+            stk.push(new int[]{price , count});
+            return count;
+        }
+        
+    }
+}
