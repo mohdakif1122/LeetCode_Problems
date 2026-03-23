2113e8a58d8a8e1ee4258edc185012b9a212f482
MOHD AKIF SIDDIQ <akifsiddiq28@gmail.com>
2026-03-23 21:30:32 +0530

    Time: 17 ms (38.63%), Space: 93.5 MB (54.39%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        Set<Integer> st = new HashSet<>();
+
+        for(int num : nums){
+            if(st.contains(num)) return true;
+            else st.add(num);
+        }
+        return false;
+    }
+}
