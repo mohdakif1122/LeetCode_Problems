6a4421df34632c15937c9311628090b89009da09
MOHD AKIF SIDDIQ <akifsiddiq28@gmail.com>
2026-04-11 02:14:04 +0530

    Time: 1 ms (99.85%), Space: 47.5 MB (89.82%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int[] temp = new int[n];
+        int count = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] != 0){
+                temp[count++] = nums[i];
+            }
+        }
+
+        for(int i = 0 ; i < count ; i++){
+            nums[i] = temp[i];
+        }
+
+        for(int i = count ; i < n ; i++){
+            nums[i] = 0;
+        }
+    }
+}
