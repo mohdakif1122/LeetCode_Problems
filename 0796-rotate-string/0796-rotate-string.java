55e4e0b4f3a663925fbb19b9c274d581be3dac4b
MOHD AKIF SIDDIQ <akifsiddiq28@gmail.com>
2026-05-04 02:37:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,23 +1,35 @@
+// class Solution {
+//     public boolean rotateString(String s, String goal) {
+//         int n = s.length();
+//         int m = goal.length();
+
+//         if (n != m) return false;
+//         if (s.equals(goal)) return true;
+
+//         StringBuilder sb = new StringBuilder(s);
+
+//         for (int i = 0; i < n; i++) {
+//             char first = sb.charAt(0);
+//             sb.deleteCharAt(0);
+//             sb.append(first);
+
+//             if (sb.toString().equals(goal)) {
+//                 return true;
+//             }
+//         }
+
+//         return false;
+//     }
+// }
+
+//========== OPTIMIZE ==========
+
 class Solution {
     public boolean rotateString(String s, String goal) {
-        int n = s.length();
-        int m = goal.length();
+        if (s.length() != goal.length()) return false;
 
-        if (n != m) return false;
-        if (s.equals(goal)) return true;
+        String combined = s + s;
 
-        StringBuilder sb = new StringBuilder(s);
-
-        for (int i = 0; i < n; i++) {
-            char first = sb.charAt(0);
-            sb.deleteCharAt(0);
-            sb.append(first);
-
-            if (sb.toString().equals(goal)) {
-                return true;
-            }
-        }
-
-        return false;
+        return combined.contains(goal);
     }
 }
