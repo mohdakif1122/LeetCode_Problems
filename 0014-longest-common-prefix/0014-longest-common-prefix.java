843f7483b7003da652dbedd4f93269ce40f6a1f7
MOHD AKIF SIDDIQ <akifsiddiq28@gmail.com>
2026-05-14 22:54:47 +0530

    Time: 5 ms (5.64%), Space: 43.4 MB (32.2%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int n = strs.length;
+        Arrays.sort(strs , Comparator.comparingInt(String::length));
+
+        String first = strs[0];
+        for(int i = 0 ; i < first.length() ; i++){
+            char ch = first.charAt(i);
+
+            for(int j = 1 ; j < n ; j++){
+                if(i >= strs[j].length() || strs[j].charAt(i) != ch) {
+                    return first.substring(0, i);
+                } 
+            }
+        }
+        return first;
+    }
+}
